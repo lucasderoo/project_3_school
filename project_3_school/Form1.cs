2e145dc1e0637000ef5c260f5375d497d778af9a
Coen Rijsdijk <0945610@hr.nl>
2018-03-28 19:04:08 +0200

    subteam1

--- project_3_school/Form1.cs
+++ project_3_school/Form1.cs
@@ -22,7 +22,7 @@ namespace project_3_school
 
         public SqlConnection create_conn(string pcname)
         {
-            SqlConnection myConnection = new SqlConnection("Data Source=" + pcname + "\\SQLEXPRESS;Initial Catalog=project_3;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlConnection myConnection = new SqlConnection("Data Source=" + pcname + "\\SQLEXPRESS;Initial Catalog=full_db_prj_3;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             myConnection.Open();
 
             return myConnection;
@@ -154,14 +154,14 @@ namespace project_3_school
 
             for (int i = 2007; i <= 2016; i++)
             {
-                cmd.CommandText = "SELECT SUM(CAST(quantity_electric as int) + CAST(quantity_hybrid as int)) FROM carByMonth  WHERE year = '" + i.ToString() + "'";
+                cmd.CommandText = "SELECT SUM(CAST(quantity_electric as int) + CAST(quantity_hybrid as int)) FROM cars_by_month_nor  WHERE year = '" + i.ToString() + "'";
                 Int32 result1 = (Int32)cmd.ExecuteScalar();
 
-                cmd.CommandText = "SELECT SUM(CAST(quantity_diesel as int)) FROM carByMonth  WHERE year = '" + i.ToString() + "'";
+                cmd.CommandText = "SELECT SUM(CAST(quantity_diesel as int)) FROM cars_by_month_nor  WHERE year = '" + i.ToString() + "'";
                 Int32 result2 = (Int32)cmd.ExecuteScalar();
 
 
-                cmd.CommandText = "SELECT SUM(CAST(quantity as int) - CAST(quantity_hybrid as int) - CAST(quantity_electric as int) - CAST(quantity_diesel as int)) FROM carByMonth  WHERE year = '" + i.ToString() + "'";
+                cmd.CommandText = "SELECT SUM(CAST(quantity as int) - CAST(quantity_hybrid as int) - CAST(quantity_electric as int) - CAST(quantity_diesel as int)) FROM cars_by_month_nor WHERE year = '" + i.ToString() + "'";
                 Int32 result3 = (Int32)cmd.ExecuteScalar();
 
                 this.ov_1_mainchart.ChartAreas["ChartArea1"].AxisX.Interval = 1;
@@ -187,10 +187,10 @@ namespace project_3_school
 
             for (int i = 2007; i <= 2016; i++)
             {
-                cmd.CommandText = "SELECT SUM(CAST(bensin_co2 as int)) FROM carByMonth  WHERE year = '" + i.ToString() + "'";
+                cmd.CommandText = "SELECT SUM(CAST(bensin_co2 as int)) FROM cars_by_month_nor  WHERE year = '" + i.ToString() + "'";
                 Int32 result1 = (Int32)cmd.ExecuteScalar();
 
-                cmd.CommandText = "SELECT SUM(CAST(diesel_co2 as int)) FROM carByMonth  WHERE year = '" + i.ToString() + "'";
+                cmd.CommandText = "SELECT SUM(CAST(diesel_co2 as int)) FROM cars_by_month_nor  WHERE year = '" + i.ToString() + "'";
                 Int32 result2 = (Int32)cmd.ExecuteScalar();
 
                 this.ov_1_mainchart.Series["Co2_benzine"].Points.AddXY(i.ToString(), result1);
@@ -232,28 +232,48 @@ namespace project_3_school
         private void ov_1_maintb_Scroll(object sender, EventArgs e)
         {
             SqlConnection con = create_conn("PC-COEN");
-
+            empty_chart(this.ov_1_mainchart);
+            this.ov_1_mainchart.ChartAreas["ChartArea1"].AxisX.Interval = 1;
+            this.ov_1_mainchart.ChartAreas.FirstOrDefault().AxisX.Interval = 1;
             SqlCommand cmd = new SqlCommand();
             cmd.CommandType = CommandType.Text;
             cmd.Connection = con;
-
-            empty_chart(ov_1_mainchart);
-            cmd.CommandText = "SELECT SUM(CAST(quantity_electric as int) + CAST(quantity_hybrid as int)) FROM carByMonth  WHERE year = '" + ov_1_maintb.Value + "'";
+            cmd.CommandText = "SELECT SUM(CAST(quantity_electric as int) + CAST(quantity_hybrid as int)) FROM cars_by_month_nor  WHERE year = '" + ov_1_maintb.Value + "'";
             Int32 result1 = (Int32)cmd.ExecuteScalar();
 
-            cmd.CommandText = "SELECT SUM(CAST(quantity_diesel as int)) FROM carByMonth  WHERE year = '" + ov_1_maintb.Value + "'";
+            cmd.CommandText = "SELECT SUM(CAST(quantity_diesel as int)) FROM cars_by_month_nor  WHERE year = '" + ov_1_maintb.Value + "'";
             Int32 result2 = (Int32)cmd.ExecuteScalar();
 
 
-            cmd.CommandText = "SELECT SUM(CAST(quantity as int) - CAST(quantity_hybrid as int) - CAST(quantity_electric as int) - CAST(quantity_diesel as int)) FROM carByMonth  WHERE year = '" + ov_1_maintb.Value + "'";
+            cmd.CommandText = "SELECT SUM(CAST(quantity as int) - CAST(quantity_hybrid as int) - CAST(quantity_electric as int) - CAST(quantity_diesel as int)) FROM cars_by_month_nor  WHERE year = '" + ov_1_maintb.Value + "'";
             Int32 result3 = (Int32)cmd.ExecuteScalar();
 
-            this.ov_1_mainchart.Series["Elektrisch/hybride"].Points.AddXY(ov_1_maintb.Value, result1);
-            this.ov_1_mainchart.Series["Diesel"].Points.AddXY(ov_1_maintb.Value, result2);
-            this.ov_1_mainchart.Series["Benzine"].Points.AddXY(ov_1_maintb.Value, result3);
-            this.ov_1_mainchart.ChartAreas["ChartArea1"].AxisX.Interval = 1;
+            this.ov_1_mainchart.Series["Elektrisch/hybride"].Points.AddXY(ov_1_maintb.Value.ToString(), result1);
+            this.ov_1_mainchart.Series["Diesel"].Points.AddXY(ov_1_maintb.Value.ToString(), result2);
+            this.ov_1_mainchart.Series["Benzine"].Points.AddXY(ov_1_maintb.Value.ToString(), result3);
+            this.ov_1_mainchart.Series["Top 1"].Points.AddXY(ov_1_maintb.Value.ToString(), 0);
+            this.ov_1_mainchart.Series["Top 2"].Points.AddXY(ov_1_maintb.Value.ToString(), 0);
+            this.ov_1_mainchart.Series["Top 3"].Points.AddXY(ov_1_maintb.Value.ToString(), 0);
 
-            
+            cmd.CommandText = "SELECT TOP 3 [make], SUM([quantity]) as quantity FROM cars_by_make_nor  WHERE year = '" + ov_1_maintb.Value + "' GROUP BY[make] ORDER BY SUM([quantity]) DESC";
+            SqlDataReader result4 = cmd.ExecuteReader();
+            int s = 0;
+            List<int> myValues = new List<int>(new int[] { 1, 2, 3 });
+
+            while (result4.Read())
+            {
+                s = s + 1;
+                this.ov_1_mainchart.Series["Top " + s].Points.AddXY(result4.GetValue(0), result4.GetValue(1));
+                foreach (var val in myValues)
+                {
+                    if (val != s)
+                    {
+                        this.ov_1_mainchart.Series["Top " + val].Points.AddXY(result4.GetValue(0), 0);
+                    }
+                }
+            }
+            ov_1_selected_year_label.Text = ov_1_maintb.Value.ToString();
+            this.ov_1_mainchart.Titles["JaarTitel"].Text = ov_1_maintb.Value.ToString();
         }
 
         private void ov_1_fullchart_button_Click(object sender, EventArgs e)
